60f5b5c00413d5777d81141b06899ef423d3c2b2
Mayur Sahare <mayursahare2024@gmail.com>
2026-06-22 09:54:27 +0530

    Fix: Update imports in utils init file after renaming pdf parser

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,6 +1,6 @@
 from .logger import setup_logger
 from .llm import get_llm_response
-from .pdf_parser import parse_pdf
+from .file_parser import parse_pdf, parse_image
 from .storage import load_memory, save_memory
 from .scoring import calculate_career_readiness
 
@@ -8,6 +8,7 @@ __all__ = [
     "setup_logger",
     "get_llm_response",
     "parse_pdf",
+    "parse_image",
     "load_memory",
     "save_memory",
     "calculate_career_readiness"
